e59d6bafc801257dbfcf8ee4f43593bea6526d07
gshivaranjani <64159053+gshivaranjani@users.noreply.github.com>
2020-08-10 09:23:37 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# RachitGoldenProject
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,53 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var gameState = 0;
+var carCode = 0;
+var form ;
+var car1,car2,car3;
+var image1,image2,image3;
+var car;
+
+var engine,world;
+
+function preload() {
+  
+}
+
+function setup() {
+  createCanvas(displayWidth-20,displayHeight-30);
+
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+  form = new Form();
+ // cars = new Cars();
+  bg = new BG(displayWidth/2, displayHeight-470, displayWidth, displayHeight);
+  ground = new Ground(displayWidth/2,displayHeight-120,displayWidth,30);
+  car = new Box(100,displayHeight-150,150,100);
+}
+
+function draw() {
+  background(255,255,255);  
+  if(gameState === 0) {
+    form.display();
+  } 
+  // else if (gameState === 1) {
+  //   form.hide();
+  //   cars.display();
+  // } 
+  else if(gameState === 1) {
+    form.hide();
+    bg.display();
+    ground.display();
+    car.display();
+
+    // bg.depth = 1;
+
+    
+  }
+  
+  
+}
